d31752581432c0488ca4b5f24e3bc1c8565ed823
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2014-01-20 09:46:41 +0900

    make velocity-dice:single value

---
+++ EvidenceBasedScheduling.Test/VelocityDice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EvidenceBasedScheduling;
+
+namespace EvidenceBasedScheduling.Test
+{
+    [TestClass]
+    public class VelocityDiceTest
+    {
+        [TestMethod]
+        public void 一つのVelocityを渡すと同じ結果が返ってくる()
+        {
+            var velocities = new[] { 1.0d };
+            var dice = new VelocityDice(velocities);
+
+            var velocity = dice.Cast();
+            velocity.Is(1.0d);
+        }
+    }
+}
---
+++ EvidenceBasedScheduling/VelocityDice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EvidenceBasedScheduling
+{
+    public class VelocityDice
+    {
+        private double[] velocities;
+
+        public VelocityDice(double[] velocities)
+        {
+            this.velocities = velocities;
+        }
+
+        public double Cast()
+        {
+            return velocities[0];
+        }
+    }
+}
