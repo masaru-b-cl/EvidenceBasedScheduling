11ab89008b85b978340ffb41efb7c5d995e5bc80
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2014-01-20 10:36:08 +0900

    simulate hour

---
+++ EvidenceBasedScheduling.Test/HourSimulatorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EvidenceBasedScheduling;
+
+namespace EvidenceBasedScheduling.Test
+{
+    [TestClass]
+    public class HourSimulatorTest
+    {
+        [TestMethod]
+        public void ランダムに選択されたVelocityによるシミュレーション結果返ってくる()
+        {
+            var dice = new VelocityDice(new[] { 1.0d, 0.5d });
+            var simulator = new HourSimulator(dice);
+
+            var estimates = new[] { 1.0d, 1.0d };
+            var actual = Enumerable.Range(1, 100).Select(_ => simulator.Simulate(estimates));
+            actual.Is(x => actual.Contains(4.0d)
+                && actual.Contains(3.0d)
+                && actual.Contains(2.0d));
+        }
+    }
+}
---
+++ EvidenceBasedScheduling/HourSimulator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EvidenceBasedScheduling
+{
+    public class HourSimulator
+    {
+        private VelocityDice dice;
+
+        public HourSimulator(VelocityDice dice)
+        {
+            this.dice = dice;
+        }
+
+        public double Simulate(double[] estimates)
+        {
+            return estimates.Sum(x => x / dice.Cast());
+        }
+    }
+}
