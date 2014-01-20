fbc7c7d6204254dc36c6dffc491acd09d248f0ac
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2014-01-20 13:25:37 +0900

    change project type "Console Application" to "Class Library"

--- EvidenceBasedScheduling/Properties/AssemblyInfo.cs
+++ EvidenceBasedScheduling/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("EvidenceBasedScheduling")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("EBS(Evidence Based Scheduling) Simulator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("TAKANO Sho")]
 [assembly: AssemblyProduct("EvidenceBasedScheduling")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © TAKANO Sho 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
